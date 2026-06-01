339773807fd91d0b5332468e926d42a9d564cf43
Antonio Ambriz <joseantonio@citio.mx>
2026-06-01 15:23:05 -0600

    Logo adaptivo: completo al expandir, recortado al colapsar, texto de vuelta

--- resources/views/_partials/macros.blade.php
+++ resources/views/_partials/macros.blade.php
@@ -1,1 +1,1 @@
-<img src="{{ asset('assets/img/branding/logo.png') }}" alt="Coprice" style="height:38px; width:auto;">
+<img src="{{ asset('assets/img/branding/logo.png') }}" alt="Coprice" class="coprice-logo" style="height:28px; width:auto; object-fit:contain; object-position:left center;">
